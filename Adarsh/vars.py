d43aacf8f7cd1222214d7184a049168ba9f16825
ANMOl5l <96918396+ANMOl5l@users.noreply.github.com>
2023-12-11 20:24:34 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,20 +9,20 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', ''))
-    API_HASH = str(getenv('API_HASH', ')
-    BOT_TOKEN = str(getenv('BOT_TOKEN', ''))
+    API_ID = int(getenv('API_ID', '26023609'))
+    API_HASH = str(getenv('API_HASH', 'ba2b67c11836d1b284e89eefecf19905')
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '6532468511:AAFi1yl2M8Z06xJQoZQ8_-yvPMG04aRIfzs'))
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', ''))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '2016820172'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5002159457").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1806353573").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Sivam_uv'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'anmol_maan'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
@@ -35,6 +35,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', ''))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'newmoveisp'))
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://anmolmaan5468:Qzk6mQa4TnoWlaQF@cluster0.s1xpg7c.mongodb.net/?retryWrites=true&w=majority'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'anmol_maan0'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
